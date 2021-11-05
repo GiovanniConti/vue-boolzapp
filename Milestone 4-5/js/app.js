1d7035495899cb9758ca251d8e770d4db398d47b
GiovanniConti <giovanni.conti@live.it>
2021-11-05 13:49:12 +0100

    added scrollToBottom to 'created'

--- Milestone 4-5/js/app.js
+++ Milestone 4-5/js/app.js
@@ -25,6 +25,36 @@ window.addEventListener("DOMContentLoaded", () =>{
               text: 'Tutto fatto!',
               status: 'received'
             },
+            {
+              date: '10/01/2020 15:30:55',
+              text: 'Hai portato a spasso il cane?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              text: 'Ricordati di dargli da mangiare',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 16:15:22',
+              text: 'Tutto fatto!',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020 15:30:55',
+              text: 'Hai portato a spasso il cane?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              text: 'Ricordati di dargli da mangiare',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 16:15:22',
+              text: 'Tutto fatto!',
+              status: 'received'
+            },
           ],
         },
         {
@@ -170,7 +200,7 @@ window.addEventListener("DOMContentLoaded", () =>{
     created(){
       this.activeChat = this.contacts[0];
       this.searchFilter();
-      // this.scrollToBottom();
+      this.scrollToBottom();
     },
 
     mounted(){
